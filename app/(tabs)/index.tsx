2a3ad31df872810f614e1a0c74d0d010543ebf8b
William Barre <william.barre@bluenav.com>
2025-09-15 14:04:37 +0200

    anchor btn

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,84 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import DeviceConnectionModal from "@/components/DeviceConnectionModal";
+import useBLE from "@/hooks/useBLE";
+import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+  const { allDevices, connectToDevice, color, sog, requestPermissions, scanForPeripherals, activateAnchor } = useBLE();
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+  const scanForDevices = async () => {
+    const isPermissionsEnabled = await requestPermissions();
+    if (isPermissionsEnabled) {
+      scanForPeripherals();
+    }
+  };
+
+  const hideModal = () => {
+    setIsModalVisible(false);
+  };
+
+  const openModal = async () => {
+    scanForDevices();
+    setIsModalVisible(true);
+  };
+
+  return (
+    <SafeAreaView style={[styles.container, { backgroundColor: color }]}>
+      {color === "white" && (
+        <TouchableOpacity onPress={openModal} style={styles.ctaButton}>
+          <Text style={styles.ctaButtonText}>Scan</Text>
+        </TouchableOpacity>
+      )}
+      {color === "yellowgreen" && (
+        <View>
+          <Text style={styles.sogText}>SOG: {sog}</Text>
+          <TouchableOpacity onPress={activateAnchor} style={styles.ctaButton}>
+            <Text style={styles.ctaButtonText}>⚓</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      <DeviceConnectionModal
+        closeModal={hideModal}
+        visible={isModalVisible}
+        connectToPeripheral={connectToDevice}
+        devices={allDevices}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: "#f2f2f2",
+    justifyContent: "center",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  ctaButton: {
+    backgroundColor: "#033e75ff",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    marginBottom: 5,
+    borderRadius: 8,
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  ctaButtonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+  sogText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "white",
+    alignSelf: "center",
+    textAlign: "center",
   },
 });
---
+++ hooks/useBLE.ts
@@ -0,0 +1,177 @@
+/* eslint-disable no-bitwise */
+import { useState } from "react";
+import { PermissionsAndroid, Platform } from "react-native";
+
+import * as ExpoDevice from "expo-device";
+
+import { BleError, BleManager, Characteristic, Device } from "react-native-ble-plx";
+
+import { Buffer } from "buffer";
+
+function base64ToByteArray(base64: string): Uint8Array {
+  return Uint8Array.from(Buffer.from(base64, "base64"));
+}
+function byteArrayToFloat64(bytes: Uint8Array, littleEndian = true): number {
+  const buffer = bytes.buffer.slice(bytes.byteOffset, bytes.byteOffset + bytes.byteLength);
+  const view = new DataView(buffer);
+  return view.getFloat64(0, littleEndian);
+}
+
+const NAVIGATION_SERVICE_UUID = "d1c8b45c-677e-4326-b41e-64d332adc505";
+const SOG_CHARACTERISTIC_UUID = "c24848da-81ac-4c14-a72d-b124ae44f9f2";
+
+const AUTOPILOT_SERVICE_UUID = "fc7de86b-25a9-4701-a808-642cd57c6e45";
+const SET_ANCHOR_CHARACTERISTIC_UUID = "d4e5f6a7-8192-0123-defa-456789012345";
+
+const bleManager = new BleManager();
+
+function useBLE() {
+  const [allDevices, setAllDevices] = useState<Device[]>([]);
+  const [connectedDevice, setConnectedDevice] = useState<Device | null>(null);
+  const [color, setColor] = useState("white");
+  const [sog, setSog] = useState("--");
+
+  const requestAndroid31Permissions = async () => {
+    const bluetoothScanPermission = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN, {
+      title: "Location Permission",
+      message: "Bluetooth Low Energy requires Location",
+      buttonPositive: "OK",
+    });
+    const bluetoothConnectPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+    const fineLocationPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+
+    return (
+      bluetoothScanPermission === "granted" &&
+      bluetoothConnectPermission === "granted" &&
+      fineLocationPermission === "granted"
+    );
+  };
+
+  const requestPermissions = async () => {
+    if (Platform.OS === "android") {
+      if ((ExpoDevice.platformApiLevel ?? -1) < 31) {
+        const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION, {
+          title: "Location Permission",
+          message: "Bluetooth Low Energy requires Location",
+          buttonPositive: "OK",
+        });
+        return granted === PermissionsAndroid.RESULTS.GRANTED;
+      } else {
+        const isAndroid31PermissionsGranted = await requestAndroid31Permissions();
+
+        return isAndroid31PermissionsGranted;
+      }
+    } else {
+      return true;
+    }
+  };
+
+  const connectToDevice = async (device: Device) => {
+    try {
+      setColor("white");
+      const deviceConnection = await bleManager.connectToDevice(device.id);
+      if (deviceConnection) {
+        await deviceConnection.discoverAllServicesAndCharacteristics();
+        setColor("green");
+        bleManager.stopDeviceScan();
+        setConnectedDevice(deviceConnection);
+        startStreamingData(deviceConnection);
+      }
+    } catch (e) {
+      console.log("FAILED TO CONNECT", e);
+      setColor("purple");
+    }
+  };
+
+  const isDuplicteDevice = (devices: Device[], nextDevice: Device) =>
+    devices.findIndex((device) => nextDevice.id === device.id) > -1;
+
+  const scanForPeripherals = () =>
+    bleManager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        console.log(error);
+      }
+
+      if (device && (device.localName === "BlueNav" || device.name === "BlueNav")) {
+        setAllDevices((prevState: Device[]) => {
+          if (!isDuplicteDevice(prevState, device)) {
+            return [...prevState, device];
+          }
+          return prevState;
+        });
+      }
+    });
+
+  const onDataUpdate = (error: BleError | null, characteristic: Characteristic | null) => {
+    // if (characteristic) {
+    //   console.log(characteristic);
+    // }
+    if (error) {
+      console.log(error);
+      setColor("red");
+      return;
+    } else if (!characteristic?.value) {
+      console.log("No Data was received");
+      setColor("yellow");
+      return;
+    } else {
+      const decodedSOG = byteArrayToFloat64(base64ToByteArray(characteristic.value)).toFixed(2);
+
+      setSog(decodedSOG.toString());
+    }
+  };
+
+  const startStreamingData = async (device: Device) => {
+    if (device) {
+      setColor("yellowgreen");
+      device.monitorCharacteristicForService(NAVIGATION_SERVICE_UUID, SOG_CHARACTERISTIC_UUID, onDataUpdate);
+    } else {
+      setColor("orange");
+      console.log("No Device Connected");
+    }
+  };
+
+  const activateAnchor = async () => {
+    if (connectedDevice) {
+      // Send the value 1 as a Float64 in base64 encoding
+      const buffer = new ArrayBuffer(8);
+      new DataView(buffer).setFloat64(0, 1, true); // little endian
+      const base64Value = Buffer.from(buffer).toString("base64");
+      connectedDevice.writeCharacteristicWithResponseForService(
+        AUTOPILOT_SERVICE_UUID,
+        SET_ANCHOR_CHARACTERISTIC_UUID,
+        base64Value
+      );
+    } else {
+      setColor("orange");
+      console.log("No Device Connected to toggle anchor");
+    }
+  };
+
+  return {
+    connectToDevice,
+    allDevices,
+    color,
+    sog,
+    requestPermissions,
+    scanForPeripherals,
+    startStreamingData,
+    activateAnchor,
+  };
+}
+
+export default useBLE;
